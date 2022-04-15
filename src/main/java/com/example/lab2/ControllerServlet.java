3dfd06103bf4bd4615fa4be8e0d7d9326839a3ec
Egor <egor_stukov@niuitmo.ru>
2022-04-15 21:02:09 +0300

    ControllerServlet done

--- src/main/java/com/example/lab2/ControllerServlet.java
+++ src/main/java/com/example/lab2/ControllerServlet.java
@@ -14,14 +14,23 @@ import java.io.PrintWriter;
 public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
+//        PrintWriter writer = resp.getWriter();
+
+        String x = req.getParameter("x_value");
+        String y = req.getParameter("y_value");
+        String r = req.getParameter("r_value");
+
+        if (x != null && y != null && r != null) {
+            String path = "/calculate";
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+            requestDispatcher.forward(req, resp);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
 
 
-        writer.write("<p>Hello, mazafaka</p>");
-        writer.close();
-//        String path = "/calculate";
-//        ServletContext servletContext = getServletContext();
-//        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
-//        requestDispatcher.forward(req, resp);
+//        writer.close();
+
     }
 }
